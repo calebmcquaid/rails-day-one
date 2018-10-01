7911975c4c56ce0226c6beecdd4c1bbe173163af
Caleb McQuaid <calebmcquaid@gmail.com>
2018-10-01 15:13:11 -0400

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def partners
+  end
+
+  def about
+  end
+
+  def search
+  end
+
+  def results
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'pages/partners'
+  get 'pages/about'
+  get 'pages/search'
+  get 'pages/results'
+  get 'pages/about'
+  get 'pages/partners'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
